62e3e217a8a6caaac7fdba1d1d7e314f1281821f
Alexis <alexisko@hawaii.edu>
2017-07-08 18:01:47 -1000

    GET method almost working?;

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 /*jshint esversion: 6 */
-const http = require("http");
-const fs = require("fs");
+const http = require('http');
+const fs = require('fs');
 
+var header, body;
 const server = http.createServer((req, res) => {
+  if(req.method === 'GET') {
+    fs.exists("."+req.url, (exists) => {
+      if(exists) {
+        res.statusCode = 200;
+        // res.setHeader('Content-Type', 'text/html');
+        body = fs.readFileSync("."+req.url, 'utf8');
+        console.log(body);
+        // res.write(body);
+        // res.end();
+      } else {
+        res.statusCode = 404;
+        body = fs.readFileSync("./public/404.html", 'utf8');
+        console.log(body);
+        // res.write(body);
+        // res.end();
+      }
+    });
+  } else if(req.method === 'POST') {
 
+  }
   res.end();
 }).listen(8080);

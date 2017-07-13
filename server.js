fb1ed2051a8b88c28ebf6297a735b654cad610a8
Alexis <alexisko@hawaii.edu>
2017-07-12 17:01:16 -1000

    Working on PUT function

--- server.js
+++ server.js
@@ -68,7 +68,6 @@ const server = http.createServer((req, res) => {
             index.splice(index.indexOf("  </ol>"), 0, newLink);
             index = index.join("\n");
             fs.writeFileSync('./public/index.html', index, 'utf8');
-            console.log(index);
           }
           //send diff status code
         });
@@ -78,10 +77,49 @@ const server = http.createServer((req, res) => {
   } else if(req.method === 'PUT') {
     fs.exists("."+req.url, (exists) => {
       if(exists) {
-        // Response body for PUT
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end('{"success" : true}');
+        var fileName, newFile, newLink;
+        req.on('data', (chunk) => { // Read in request body
+          body = chunk.toString();
+          body = queryString.parse(body);
+          fileName = body.elementName.toLowerCase() + ".html";
+          fs.exists("./public/"+fileName, (exists) => {
+            if(!exists) { // If the file does ot exist, create a new file
+              newFile = `<!DOCTYPE html>
+              <html lang="en">
+              <head>
+                <meta charset="UTF-8">
+                <title>The Elements - ${body.elementName}</title>
+                <link rel="stylesheet" href="/public/css/styles.css">
+              </head>
+              <body>
+                <h1>${body.elementName}</h1>
+                <h2>${body.elementSymbol}</h2>
+                <h3>Atomic number ${body.elementAtomicNumber}</h3>
+                <p>${body.elementDescription}</p>
+                <p><a href="/public/index.html">back</a></p>
+              </body>
+              </html>`;
+              fs.writeFile('./public/'+fileName, newFile, (err) => { // Create file
+                if(err) throw err;
+              });
+              // Response body for POST
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.end('{"success" : true}');
+              // Update index.html
+              index = fs.readFileSync('./public/index.html', 'utf8');
+              index = index.split("\n");
+              newLink =  `
+              <li>
+                <a href="public/${fileName}">${body.elementName}</a>
+              </li>`;
+              index.splice(index.indexOf("  </ol>"), 0, newLink);
+              index = index.join("\n");
+              fs.writeFileSync('./public/index.html', index, 'utf8');
+            }
+            //send diff status code
+          });
+        });
       } else {
         // Response body for PUT
         res.statusCode = 500;
@@ -93,10 +131,12 @@ const server = http.createServer((req, res) => {
   } else if(req.method === 'DELETE') {
     fs.exists("."+req.url, (exists) => {
       if(exists) {
-        // Response body for DELETE
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end('{"success" : true}');
+        fs.unlink("."+req.url, () => {
+          // Response body for DELETE
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end('{"success" : true}');
+        });
       } else {
         // Response body for PUT
         res.statusCode = 500;

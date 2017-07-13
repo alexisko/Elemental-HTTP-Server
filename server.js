9b3c2c17d83de9cf9bbdec5d660d4c909ccffa34
Alexis <alexisko@hawaii.edu>
2017-07-12 17:18:15 -1000

    PUT function working for the most part

--- server.js
+++ server.js
@@ -72,60 +72,62 @@ const server = http.createServer((req, res) => {
           //send diff status code
         });
       });
+    } else {
+      // Response body for PUT
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(`{ "error" : "resource ${req.url} does not exist" }`);
     }
   //PUT METHOD
   } else if(req.method === 'PUT') {
-    fs.exists("."+req.url, (exists) => {
-      if(exists) {
-        var fileName, newFile, newLink;
-        req.on('data', (chunk) => { // Read in request body
-          body = chunk.toString();
-          body = queryString.parse(body);
-          fileName = body.elementName.toLowerCase() + ".html";
-          fs.exists("./public/"+fileName, (exists) => {
-            if(!exists) { // If the file does ot exist, create a new file
-              newFile = `<!DOCTYPE html>
-              <html lang="en">
-              <head>
-                <meta charset="UTF-8">
-                <title>The Elements - ${body.elementName}</title>
-                <link rel="stylesheet" href="/public/css/styles.css">
-              </head>
-              <body>
-                <h1>${body.elementName}</h1>
-                <h2>${body.elementSymbol}</h2>
-                <h3>Atomic number ${body.elementAtomicNumber}</h3>
-                <p>${body.elementDescription}</p>
-                <p><a href="/public/index.html">back</a></p>
-              </body>
-              </html>`;
-              fs.writeFile('./public/'+fileName, newFile, (err) => { // Create file
-                if(err) throw err;
-              });
-              // Response body for POST
-              res.statusCode = 200;
-              res.setHeader('Content-Type', 'application/json');
-              res.end('{"success" : true}');
-              // Update index.html
-              index = fs.readFileSync('./public/index.html', 'utf8');
-              index = index.split("\n");
-              newLink =  `
-              <li>
-                <a href="public/${fileName}">${body.elementName}</a>
-              </li>`;
-              index.splice(index.indexOf("  </ol>"), 0, newLink);
-              index = index.join("\n");
-              fs.writeFileSync('./public/index.html', index, 'utf8');
-            }
-            //send diff status code
+    req.on('data', (chunk) => {
+      body = chunk.toString();
+      body = queryString.parse(body);
+      fs.exists("."+req.url, (exists) => {
+        if(exists || !exists) {
+          newFile = `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>The Elements - ${body.elementName}</title>
+            <link rel="stylesheet" href="/public/css/styles.css">
+          </head>
+          <body>
+            <h1>${body.elementName}</h1>
+            <h2>${body.elementSymbol}</h2>
+            <h3>Atomic number ${body.elementAtomicNumber}</h3>
+            <p>${body.elementDescription}</p>
+            <p><a href="/public/index.html">back</a></p>
+          </body>
+          </html>`;
+          fs.writeFile("."+req.url, newFile, (err) => { // Create file
+            if(err) throw err;
           });
-        });
-      } else {
-        // Response body for PUT
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(`{ "error" : "resource ${req.url} does not exist" }`);
-      }
+          // Response body for POST
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end('{"success" : true}');
+          if(exists) {
+            //
+          } else { //file doesn't exist create new link
+            // Update index.html
+            index = fs.readFileSync('./public/index.html', 'utf8');
+            index = index.split("\n");
+            newLink =  `
+            <li>
+              <a href="public/${fileName}">${body.elementName}</a>
+            </li>`;
+            index.splice(index.indexOf("  </ol>"), 0, newLink);
+            index = index.join("\n");
+            fs.writeFileSync('./public/index.html', index, 'utf8');
+          }
+        } else {
+          // Response body for PUT
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(`{ "error" : "resource ${req.url} does not exist" }`);
+        }
+      });
     });
   //DELETE METHOD
   } else if(req.method === 'DELETE') {
